c10666b028110250d456e3b68dbc23ac0f2183f2
Alec Lang <lostlang@icloud.com>
2024-05-02 10:59:22 +0300

    feat: add new package and clipboard manager

--- wls.sh
+++ wls.sh
@@ -13,10 +13,16 @@ pacman-key --refresh-keys
 sudo pacman -Syu
 
 # Install packages
-pacman -Sy fish neovim neofetch openssh go npm xclip
+pacman -Sy fish neovim neofetch openssh go npm xclip unzip ripgrep htop gcc make deno rustup rust-analyzer
 
 # Set fish as default
 chsh -s /usr/bin/fish
+
+# Work with windows cp buffer
+curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+mv /tmp/win32yank.exe /usr/local/bin/
 
 # Exit sudo -s
 exit
@@ -24,4 +30,5 @@ exit
 # Git config
 git config --global user.email "lostlang@icloud.com"
 git config --global user.name "Alec Lang"
+git config --global core.editor nvim
 
