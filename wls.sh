78fc3d87a61c16e29dcd361877bb74b9b69ef7d5
Alec Lang <lostlang@icloud.com>
2024-08-12 04:24:49 +0500

    feat: update config

--- wls.sh
+++ wls.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Go root
-sudo -s
-
-# Update keys
-archlinux-keyring-wkd-sync
-pacman-key --init
-pacman-key --populate archlinux
-pacman-key --refresh-keys
-
 # Update system
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 # Install packages
-pacman -Sy fish neovim neofetch openssh go npm xclip unzip ripgrep htop gcc make deno rustup rust-analyzer
+sudo pacman -Syu --noconfirm openssh zip unzip inetutils
+sudo pacman -Syu --noconfirm fish zellij neovim neofetch htop
+sudo pacman -Syu --noconfirm python go npm rustup
 
-# Set fish as default
-chsh -s /usr/bin/fish
+# Setup fish
+sudo chsh -s /usr/bin/fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bira
+cp -r omf $HOME/.config
 
-# Work with windows cp buffer
-curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
-unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
-chmod +x /tmp/win32yank.exe
-mv /tmp/win32yank.exe /usr/local/bin/
+# Setup zellij
+cp -r zellij $HOME/.config
 
-# Exit sudo -s
-exit
+# Setup nvim
+git clone https://github.com/lostlang/NvimConfig.git $HOME/.config/nvim
+git clone https://aur.archlinux.org/golangci-lint.git golangci-lint
+cd golangci-lint
+makepkg -sic --noconfirm
+cd ..
+
+# Setup rust
+rustup default stable
 
 # Git config
 git config --global user.email "lostlang@icloud.com"
